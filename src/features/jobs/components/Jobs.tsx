9f37b4cfb8d5061612ba18911b8875d48843c333
jsemalvarez <jsemalvarez@gmail.com>
2024-10-22 12:58:38 -0300

    refactor: update css jobs

--- src/features/jobs/components/Jobs.tsx
+++ src/features/jobs/components/Jobs.tsx
@@ -10,7 +10,7 @@ export const Jobs = () => {
         <div className='job-card'>
           <div className='job-card__description-bg'></div>
           <div className='job-card__description-info'>
-            <h3>conexa</h3>
+            <h3>Conexa</h3>
             <p><span className='job-card__title'>Node Developer</span> - jornada completa</p>
             <p>12/2021 - 12/2023</p>
             <p>Siendo parte del equipo de Integraciones, mi función principal como <strong>desarrollador Node</strong> es conectar la plataforma ecommerce <strong>Shopify</strong> con diversos medios de envío y pago, permitiendo que estén disponibles como opciones en el proceso de pago o de envío. He tenido la oportunidad de integrar a clientes como <strong>MODO</strong>, <strong>PedidosYa</strong>, <strong>Cabify</strong>, entre otros. Además de estas integraciones, me encargue de crear, mantener y mejorar un boilerplate para agilizar la integración de los medios de envío, así como mantener y mejorar otro boilerplate para los medios de pago. También hemos implementado el uso de TypeScript en nuestros proyectos más recientes, mejorando la calidad y mantenibilidad del código.</p>
@@ -40,7 +40,7 @@ export const Jobs = () => {
             <h3>Maker</h3>
             <p><span className='job-card__title'>Full Stack Developer</span> - jornada completa</p>
             <p>10/2019 - 12/2021</p>
-            <p>Mis tareas principales, Full Stack Developer, incluían la actualización y desarrollo de nuevas funcionalidades de un Dashboard Web para la gestión de viajes, operadores, choferes y control vehicular
+            <p>Mis tareas principales como Full Stack Developer, incluían la actualización y desarrollo de nuevas funcionalidades de un Dashboard Web para la gestión de viajes, operadores, choferes y control vehicular
             con seguimiento en tiempo real. También tuve la oportunidad de diseñar y desarrollar un Sistema web para la gestión autónoma e integral de viajes corporativos, también con seguimiento en tiempo real.</p>
           </div>
           <div className='job-card__img job-card__img--maker'></div>
